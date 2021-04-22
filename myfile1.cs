5d2aa0a79baf77c4f5a55ae7bf9416a05444b2ea
unknown <santosh.kumar13698@gmail.com>
2021-04-22 11:33:29 +0530

    first commit

---
+++ myfile1.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Write("santosh");
+        Console.Write("kumar");
+        int a=6,d=8;
+        Console.Write(a+d+"\n");
+        Console.Write(a-d+"\n");
+        Console.Write(a*d+"\n");
+        Console.Write(a==d);
+        Console.Write("\n"+a/d);
+        
+        ///sum
+        ///minus
+        ///multiply
+        ///equality
+        ///Divide
